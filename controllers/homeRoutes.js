170b489e0e7a1095f7c842b004c509426e910cbf
BallinGuy <tballin2000@hotmail.com>
2022-11-11 12:27:25 -0500

    changed some names

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -72,7 +72,7 @@ router.get('/profile', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('/search');
     return;
   }
 
