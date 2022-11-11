97898c28e720fef4644f021fb709a9cf04f77a90
PeteChicchetti <pj0524@gmail.com>
2022-11-11 17:50:46 -0500

    restrucred header for login but username not dynamic

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,22 +1,25 @@
-module.exports = {
-  format_date: (date) => {
-    // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
-  },
-  format_amount: (amount) => {
-    // format large numbers with commas
-    return parseInt(amount).toLocaleString();
-  },
-  get_emoji: () => {
-    const randomNum = Math.random();
+const { response } = require("express");
 
-    // Return a random emoji
-    if (randomNum > 0.7) {
-      return `<span for="img" aria-label="lightbulb">💡</span>`;
-    } else if (randomNum > 0.4) {
-      return `<span for="img" aria-label="laptop">💻</span>`;
-    } else {
-      return `<span for="img" aria-label="gear">⚙️</span>`;
-    }
-  },
+module.exports = {
+  // format_date: (date) => {
+  //   // Format date as MM/DD/YYYY
+  //   return date.toLocaleDateString();
+  // },
+  // format_amount: (amount) => {
+  //   // format large numbers with commas
+  //   return parseInt(amount).toLocaleString();
+  // },
+  // username: () => {
+  //   const user = fetch('/api/users/', {
+  //     method: 'GET',
+
+  //   })
+  //   .then(function (response) {
+  //     return response.json();
+  //   })
+  //   .then(function (data) {
+  //     console.log(data)
+  //   })
+  // }
 };
+
