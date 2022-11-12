def12814a2f6b52375e48b2689951ec8aca60fb8
PeteChicchetti <pj0524@gmail.com>
2022-11-12 12:04:50 -0500

    added functional logout button

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -29,6 +29,7 @@ router.post('/', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
+      
 
       res.status(200).json(newUser);
     });
