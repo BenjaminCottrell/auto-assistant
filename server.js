360dec7f42caa0e31e65f36efc72998f6cf2d9c3
PeteChicchetti <pj0524@gmail.com>
2022-11-14 15:47:46 -0500

    edit cookie session time

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const hbs = exphbs.create({ helpers });
 const sess = {
   secret: 'Super secret secret',
   cookie: {
-    maxAge: 300000,
+    maxAge: 60 * 60 * 1000,
     httpOnly: true,
     secure: false,
     sameSite: 'strict',
