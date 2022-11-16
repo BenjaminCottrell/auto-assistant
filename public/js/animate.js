dcf1db9b9be8c9a41f2171157c35b14920bfce78
BenCottrell <bencottrell48@yahoo.com>
2022-11-16 13:56:23 -0500

    hearts animate separately

--- public/js/animate.js
+++
@@ -1,5 +0,0 @@
-const heartbutton = document.querySelectorAll('.results-heart');
-
-heartbutton.forEach(function(element) {
-    addEventListener('click', () => 
-    element.classList.add('animate__animated', 'animate__heartBeat'))});
