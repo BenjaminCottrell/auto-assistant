be4b9d9d68c534eaf1250801fb46d0c2ccd98e26
BallinGuy <tballin2000@hotmail.com>
2022-11-16 13:37:12 -0500

    dom traversal works properly, needs css

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,14 +1,14 @@
 const saveButtonHandeler = async (event) =>{
     event.preventDefault();
     console.log("It works?");
-    let id = event.target.parentElement.parentElement.parentElement.children[0].children[0].children[1].children[0].id;
-    let make = event.target.parentElement.parentElement.parentElement.children[0].children[1].children[0].children[0].innerText;
-    let model = event.target.parentElement.parentElement.parentElement.children[0].children[1].children[0].children[1].innerText;
-    let year = event.target.parentElement.parentElement.parentElement.children[0].children[1].children[0].children[2].innerText;
-    let drive = event.target.parentElement.parentElement.parentElement.children[0].children[1].children[1].children[0].innerText;
-    let trans = event.target.parentElement.parentElement.parentElement.children[0].children[1].children[1].children[1].innerText;
-    let cylinders = event.target.parentElement.parentElement.parentElement.children[0].children[1].children[2].children[0].innerText;
-    let mpg = event.target.parentElement.parentElement.parentElement.children[0].children[1].children[2].children[1].innerText;
+    let id = event.target.parentElement.parentElement.parentElement.children[0].children[1].children[0].id;
+    let make = event.target.parentElement.parentElement.parentElement.children[1].children[0].children[0].innerText;
+    let model = event.target.parentElement.parentElement.parentElement.children[1].children[0].children[1].innerText;
+    let year = event.target.parentElement.parentElement.parentElement.children[1].children[0].children[2].innerText;
+    let drive = event.target.parentElement.parentElement.parentElement.children[1].children[1].children[0].innerText;
+    let trans = event.target.parentElement.parentElement.parentElement.children[1].children[1].children[1].innerText;
+    let cylinders = event.target.parentElement.parentElement.parentElement.children[1].children[2].children[0].innerText;
+    let mpg = event.target.parentElement.parentElement.parentElement.children[1].children[2].children[1].innerText;
     let notes = event.target.parentElement.parentElement.children[0].value;
     console.log(id, make, model, year, drive, trans, cylinders, mpg, notes);
     const response = await fetch(`/api/favorites/${id}`, {
