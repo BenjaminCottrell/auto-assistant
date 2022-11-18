f307c2ddc7cf05019742a042e4b5ba26205f7b61
BenCottrell <bencottrell48@yahoo.com>
2022-11-18 14:00:16 -0500

    I am truly an id10t, helpers was used in the exphbs.create and was no longer defined after the require was removed

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create();
 
 const sess = {
   secret: 'Super secret secret',
