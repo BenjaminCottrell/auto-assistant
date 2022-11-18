5cfdc5b54ba04d2b62c74e37fc333b99df26d11f
BenCottrell <bencottrell48@yahoo.com>
2022-11-18 13:44:31 -0500

    server.js still required a helpers file I deleted, which was not used, removed require for it

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
 
 
 const sequelize = require('./config/connection');
